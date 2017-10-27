37ff3e2f73b5ed3d44c826fbf2c67eeff93468b1
Jamal Rasool <VeveCorp@users.noreply.github.com>
2017-10-26 19:58:28 -0500

    Updating Information for shippingStoreTest Class
    Signed-off-by: Trujamal <jamal.rasool@outlook.com>

--- src/ShippingStoreTest.java
+++ src/ShippingStoreTest.java
@@ -2,6 +2,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import static org.junit.Assert.*;
 
@@ -18,6 +20,8 @@ public class ShippingStoreTest {
     ShippingStore shippingStore;
     File dataFile;
     ArrayList<PackageOrder> packageOrderList;
+    PrintWriter pw;
+
 
     // Initalizing base packages to be made.
     PackageOrder defaultPack1;
@@ -32,6 +36,7 @@ public class ShippingStoreTest {
         dataFile = new File("PackageOrderDB.txt");
         shippingStore = new ShippingStore();
         packageOrderList = new ArrayList<>();
+         pw = new PrintWriter("PackageOrderDB.txt");
 
         defaultPack1 = new PackageOrder("12345","Box", "Metro", "First", 5.00f, 53);
         defaultPack2 = new PackageOrder("54321","Envelope", "Metro", "First", 4.00f, 53);
@@ -41,8 +46,10 @@ public class ShippingStoreTest {
         packageOrderList.add(defaultPack1);
         packageOrderList.add(defaultPack2);
         packageOrderList.add(defaultPack3);
-        // Created the new information
 
+        for (PackageOrder c : packageOrderList) {
+            pw.write(c.toString());
+        }
 
     }
 
@@ -98,11 +105,10 @@ public class ShippingStoreTest {
      */
     @Test
     public void findPackageOrder() throws Exception {
-        // Testing to see if it can find it or not by returning the boolean value.
-        assertTrue("The package has been located",
+        // Testing to see if it exist or does not exist
+       assertTrue("The package exist",
                 (packageOrderList.get(0).getTrackingNumber()).matches("12345"));
-
-        assertFalse("The package is not inside of the array",
+        assertFalse("The package does not exist",
                 (packageOrderList.get(0).getTrackingNumber()).matches("00000"));
     }
 
@@ -112,7 +118,12 @@ public class ShippingStoreTest {
      */
     @Test
     public void searchPackageOrder() throws Exception {
-        assertEquals("Find me another one", );
+        // Check to see if it contains the tracking number in the output
+        assertTrue("The package has been located in array",
+                (packageOrderList.get(0).getTrackingNumber()).matches("12345"));
+
+        assertFalse("The package is not inside of the array",
+                (packageOrderList.get(0).getTrackingNumber()).matches("00000"));
     }
 
     /**
@@ -131,6 +142,8 @@ public class ShippingStoreTest {
      */
     @Test
     public void removeOrder() throws Exception {
+        packageOrderList.remove(defaultPack3);
+        assertEquals(2, packageOrderList.size());
     }
 
     /**
@@ -139,6 +152,9 @@ public class ShippingStoreTest {
      */
     @Test
     public void getPackageOrder() throws Exception {
+        // Testing to see if it can get the package and return said information.
+       assertTrue("this object exist",packageOrderList.get(0).equals(defaultPack1));
+       assertFalse("this object doesn't exist", packageOrderList.get(1).equals(defaultPack1));
     }
 
     /**
@@ -147,6 +163,9 @@ public class ShippingStoreTest {
      */
     @Test
     public void read() throws Exception {
+        // Checking to see if what we wrote to the file was written properly
+
+
     }
 
     /**
@@ -155,6 +174,9 @@ public class ShippingStoreTest {
      */
     @Test
     public void flush() throws Exception {
+        //String reader/string writer
+        assertTrue("f", pw.equals(defaultPack1));
+
     }
 
 }
